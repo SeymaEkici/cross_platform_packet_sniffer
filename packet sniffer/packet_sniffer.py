89b63b4d56fb52d62063e80afe53081f6efb2343
Şeyma Ekici <100785004+SeymaEkici@users.noreply.github.com>
2024-04-30 18:09:39 +0300

    Update packet_sniffer.py

--- packet sniffer/packet_sniffer.py
+++ packet sniffer/packet_sniffer.py
@@ -1,21 +1,22 @@
 import platform
 from scapy.all import sniff
 
-PACKET_COUNT = 50  # Durmak için dinlenecek paket sayısı
+PACKET_COUNT = 50  # Number of packets to sniff before stopping
 
 def packet_callback(packet):
-    packet.show()  # Paketin tamamını göster
+    packet.show()  # Show the entire packet
 
 def start_sniffing():
     print("Starting packet sniffer...")
     if platform.system() == "Windows":
-        # Windows'ta, Npcap ile çalışmak için promisc parametresini True olarak ayarlayarak sniff fonksiyonunu kullanın
+        # On Windows, use the sniff function with promisc set to True to work with Npcap
         sniff(prn=packet_callback, store=0, iface=None, promisc=True, count=PACKET_COUNT)
     elif platform.system() == "Darwin":
-        # macOS için ek bir import gerekli değil
+        # No additional imports required for macOS
+        sniff(prn=packet_callback, store=0, iface=None, count=PACKET_COUNT)
         pass
     elif platform.system() == "Linux":
-        # Linux için ek bir import gerekli değil
+        # No additional imports required for Linux
         sniff(prn=packet_callback, store=0, iface=None, count=PACKET_COUNT)
     else:
         print("Unsupported operating system")
